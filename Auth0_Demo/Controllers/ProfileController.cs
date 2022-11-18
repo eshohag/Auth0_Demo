759ab956c840c7a3357a1f5053755305b34f2470
eshoh <eshoh@SHOHAG>
2022-11-18 20:59:47 +0600

    Add project files.

---
+++ Auth0_Demo/Controllers/ProfileController.cs
@@ -0,0 +1,26 @@
+﻿using Auth0_Demo.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Auth0_Demo.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var claims = User.Claims.FirstOrDefault();
+            var data = new UserProfileViewModel
+            {
+                Name = User.Identity.Name,
+                EmailAddress = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value,
+                ProfileImage = User.Claims.FirstOrDefault(c => c.Type == "picture")?.Value,
+                AccessToken = accessToken
+            };
+            return View(data);
+        }
+    }
+}
---
+++ Auth0_Demo/Models/UserProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Auth0_Demo.Models
+{
+    public class UserProfileViewModel
+    {
+        public string Name { get; set; } = null!;
+        public string EmailAddress { get; set; } = null!;
+        public string ProfileImage { get; set; } = null!;
+        public string AccessToken { get; set; } = null!;
+    }
+}
